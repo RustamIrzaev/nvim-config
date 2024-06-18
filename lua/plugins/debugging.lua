4c31ac09a5b133e7a668634ec37d00003a1327fe
Rustam <irzaevrustam@gmail.com>
2024-06-18 20:02:23 +0200

    added a little more for c# support

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -3,10 +3,14 @@ return {
 	dependencies = {
 		"rcarriga/nvim-dap-ui",
 		"nvim-neotest/nvim-nio",
+		"Cliffback/netcoredbg-macOS-arm64.nvim",
 	},
 	config = function()
 		local dap = require("dap")
 		local dapui = require("dapui")
+		local netcoredbg = require("netcoredbg-macOS-arm64")
+
+		netcoredbg.setup(dap)
 
 		dap.listeners.before.attach.dapui_config = function()
 			dapui.open()
@@ -20,6 +24,21 @@ return {
 		dap.listeners.before.event_exited.dapui_config = function()
 			dapui.close()
 		end
+
+		dap.adapters.coreclr = {
+			type = "executable",
+			command = netcoredbg,
+			args = { "--interpreter=vscode" },
+		}
+
+		dap.configurations.cs = {
+			type = "coreclr",
+			name = "launch - netcoredbg",
+			request = "launch",
+			program = function()
+				return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+			end,
+		}
 
 		vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
 		vim.keymap.set("n", "<leader>dc", dap.continue, {})
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,43 +1,51 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "csharp_ls", "rust_analyzer", "tsserver" },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "csharp_ls", "rust_analyzer", "tsserver" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			local roslyn = require("roslyn")
 
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.tsserver.setup({
-        capabilities = capabilities,
-      })
-      --lspconfig.rust_analyzer.setup({
-      --  capabilities = capabilities,
-      --  settings = {
-      --    ["rust-analyzer"] = {
-      --      check = { command = "clippy" },
-      --      diagnostics = { enable = true },
-      --    },
-      --  },
-      --})
+			roslyn.setup({
+				dotnet_cmd = "dotnet",
+				roslyn_version = "4.8.0-3.23475.7",
+				on_attach = function() end,
+				capabilities = capabilities,
+			})
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			--lspconfig.rust_analyzer.setup({
+			--  capabilities = capabilities,
+			--  settings = {
+			--    ["rust-analyzer"] = {
+			--      check = { command = "clippy" },
+			--      diagnostics = { enable = true },
+			--    },
+			--  },
+			--})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
